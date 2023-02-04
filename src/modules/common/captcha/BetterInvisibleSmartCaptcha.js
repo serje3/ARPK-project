b9e28c52c2b89dec9a1c773fc540cc818677577e
serje3 <serjerin322@gmail.com>
2023-02-04 12:40:57 +0300

    zaebalo

--- src/modules/common/captcha/BetterInvisibleSmartCaptcha.js
+++ src/modules/common/captcha/BetterInvisibleSmartCaptcha.js
@@ -1,4 +1,4 @@
-import { memo, useCallback, useEffect, useState } from "react";
+import {InvisibleSmartCaptcha} from "@yandex/smart-captcha";
 
 
 const BetterInvisibleSmartCaptcha = ({
@@ -10,48 +10,52 @@ const BetterInvisibleSmartCaptcha = ({
                                          visible,
                                          hideShield
                                      }) => {
-    const containerClass = "smart-captcha"
-    const containerId = "smart-captcha-container-id"
-    const callback = useCallback((token) => onSuccess(token), [onSuccess])
-    const [widgetId, setWidgetId] = useState(null)
+    // const containerClass = "smart-captcha"
+    // const containerId = "smart-captcha-container-id"
+    // const callback = useCallback((token) => onSuccess(token), [onSuccess])
+    // const [widgetId, setWidgetId] = useState(null)
 
 
-    useEffect(() => {
-        if (!window.smartCaptcha)
-            return
-        if (visible && widgetId !== null) {
-            window.smartCaptcha.execute(widgetId)
-            onChallengeHidden()
-        }
-
-        return () => {
-        };
-    }, [onChallengeHidden, visible, widgetId])
-
-    useEffect(() => {
-        if (!window.smartCaptcha || !sitekey)
-            return;
-        const widget = window.smartCaptcha.render(containerId, {
-            sitekey: sitekey,
-            invisible: true,
-            callback: callback,
-            hideShield: hideShield
-        })
-        setWidgetId(widget)
-
-
-        return () => {
-            window.smartCaptcha.destroy(widget)
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+    // useEffect(() => {
+    //     if (!window.smartCaptcha)
+    //         return
+    //     if (visible && widgetId !== null) {
+    //         window.smartCaptcha.execute(widgetId)
+    //         onChallengeHidden()
+    //     }
+    //
+    //     return () => {
+    //     };
+    // }, [onChallengeHidden, visible, widgetId])
+    //
+    // useEffect(() => {
+    //     if (!window.smartCaptcha || !sitekey)
+    //         return;
+    //     const widget = window.smartCaptcha.render(containerId, {
+    //         sitekey: sitekey,
+    //         invisible: true,
+    //         callback: callback,
+    //         hideShield: hideShield
+    //     })
+    //     setWidgetId(widget)
+    //
+    //
+    //     return () => {
+    //         window.smartCaptcha.destroy(widget)
+    //     }
+    //     // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, [])
 
     return (
-        <div
-            id={containerId}
-            className={containerClass}
-        />
+        <InvisibleSmartCaptcha
+            sitekey={sitekey}
+            onSuccess={onSuccess}
+            onChallengeVisible={onChallengeVisible}
+            onChallengeHidden={onChallengeHidden}
+            onNetworkError={onNetworkError}
+            visible={visible}
+            hideShield={hideShield}/>
     )
 }
 
-export default memo(BetterInvisibleSmartCaptcha)
+export default BetterInvisibleSmartCaptcha
